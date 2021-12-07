ddc0cf3be8f583f5fb01610d0ce42d174ffe75da
karthik <93367753+karthik1reddy@users.noreply.github.com>
2021-12-07 14:05:13 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main()
+{
+  int r,n,sum=0,temp;
+	printf("enter the 3 digits  number");
+  scanf("%d",&n);
+  temp=n;
+  while(n>0)
+{
+  r=n%10;
+  sum=sum+(r*r*r);
+  n=n/10;
+}
+ if(temp==sum)
+ printf("armstrong number");
+ else
+ printf ("not armstrong number");
+}
