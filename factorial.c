3ebcac2e49f4a01c91ef88625a7b946a47e3572f
karthik <93367753+karthik1reddy@users.noreply.github.com>
2021-12-10 06:50:04 +0000

    factorial.c

--- factorial.c
+++ factorial.c
@@ -2,13 +2,13 @@
 int main(){
     int num,i,f=1;
     scanf("%d",&num);
-    if(num<=10){
-         for(i=1;i<=10;i++){
+    if(num<10 && num>=0){
+         for(i=1;i<=num;i++){
           f=f*i;
-          printf("%d ",f);
         }
+         printf("%d ",f);
     }
-    else if(num>10){
+    else if(num>=10){
         printf("Too big a number");
     }
     else{
