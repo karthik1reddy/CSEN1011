d3ed6729e8ab3f6697899b1b766971f0b6fadaa4
karthik <93367753+karthik1reddy@users.noreply.github.com>
2022-02-25 02:55:58 +0000

    few changes

--- chess.c
+++ chess.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main ()
 {
-   int A,B,C,D,solution;
+   int A,B,C,D,sum;
    printf("enter the first row and first column");
    scanf("%d %d ",&A,&B);
    printf("enter the second row and second column");
    scanf("%d %d",&C,&D);
-   solution=A+B+C+D;
-   if(solution%2==0)
+   sum=A+B+C+D;
+   if(sum%2==0)
    {
         printf("True");
     }
